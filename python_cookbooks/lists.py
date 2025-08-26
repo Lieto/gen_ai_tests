5e89915764a5c9559359504d2570c48861863561
Lieto <vkuoppala@hotmail.com>
2025-08-26 15:18:19 +0000

    indexing and slicing

--- python_cookbooks/lists.py
+++ python_cookbooks/lists.py
@@ -29,5 +29,22 @@ print(numbers_copy)
 natural_numbers = numbers[1:]
 print(natural_numbers)
 
+# Stop index can overflow safely
+natural_numbers = numbers[1:100]
+print(natural_numbers)
+
+# On sepcifying stop, the element at the stop index is excluded
+natural_numbers_upto_5 = numbers[1:6]
+print(natural_numbers_upto_5)
+
+odd_numbers = numbers[1::2]
+print(odd_numbers)
+
+reversed_numbers = numbers[::-1]
+print(reversed_numbers)
+
+reversed_even_numbers = numbers[-1::-2]
+print(reversed_even_numbers)
+
 
 
